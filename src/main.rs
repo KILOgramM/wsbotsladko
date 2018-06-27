3913b252edcf11dd0cb09b2a691adce286421818
Adler <gameved@users.noreply.github.com>
2018-06-27 19:57:31 +0300

    Add print Ready event

--- src/main.rs
+++ src/main.rs
@@ -2732,7 +2732,9 @@ fn main() {
                     }
                 });
             }
-
+            Event::Ready(json) => {
+                println!("READY:\n{}", serde_json::to_string_pretty(&json));
+            }
             _ => {}
 
             //END OF MAIN THREAD
